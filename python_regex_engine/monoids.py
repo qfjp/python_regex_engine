3744e6fdf2e6292763f7816cf2a83a310eee7280
qfjp <qfjp@users.noreply.github.com>
2025-02-18 18:07:47 -0500

    Fill out definition of a `Set` monoid
    - Constructor can convert lists or tuples
    - Add difference, subtraction overloading, format, and hash methods

--- python_regex_engine/monoids.py
+++ python_regex_engine/monoids.py
@@ -4,9 +4,9 @@ from pymonad.monoid import Monoid  # type: ignore[import-untyped]
 
 
 class Set[T](Monoid):  # type: ignore[no-any-unimported, misc]
-    def __init__(self, val: set[T] | T = set()) -> None:
+    def __init__(self, val: set[T] | list[T] | tuple[T] | T = set()) -> None:
         self.value: set[T]
-        if isinstance(val, set) or isinstance(val, list):
+        if isinstance(val, set) or isinstance(val, list) or isinstance(val, tuple):
             self.value = set(val)
         else:
             self.value = {val}
@@ -46,11 +46,20 @@ class Set[T](Monoid):  # type: ignore[no-any-unimported, misc]
     def add(self, x: T) -> None:
         self.value.add(x)
 
+    def difference(self, x: Self) -> Self:
+        return Set(self.value - x.value)
+
+    def __sub__(self, x: Self) -> Self:
+        return self.difference(x)
+
     def __iter__(self) -> Iterator[T]:
         return self.value.__iter__()
 
     def __len__(self) -> int:
         return len(self.value)
+
+    def __format__(self, format_spec: str) -> str:
+        return ("{:" + format_spec + "}").format(str(self))
 
     def __repr__(self) -> str:
         return "{}".format(self.value)
@@ -61,6 +70,14 @@ class Set[T](Monoid):  # type: ignore[no-any-unimported, misc]
         elif isinstance(other, set):
             return self.value == other
         return False
+
+    def __hash__(self: Self) -> int:
+        """
+        A DANGEROUS thing to do, if we are using this function
+        (which is done implicitly in dict keys) we need to make
+        sure that the set itself doesn't change.
+        """
+        return hash(tuple(self.value))
 
 
 class Sum(Monoid[int]):  # type: ignore[no-any-unimported, misc]
